ba557786ed7ba71c45c5237b812368a7a0e3e58e
pandresvaldivia <pandresvaldiviacuzcano@gmail.com>
2022-01-07 19:03:25 -0500

    Task cell and Task component completed

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import '../styles/main.css';
 import '../styles/icons.css';
 import '../styles/page.css';
+import '../styles/task.css';
+import '../styles/task-cell.css';
 import '../styles/calendar.css';
