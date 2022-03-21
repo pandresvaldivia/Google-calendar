4340e6c765391d5e79152cddead976f74504dfc2
pandresvaldivia <pandresvaldiviacuzcano@gmail.com>
2022-03-20 20:38:02 -0500

    Styles files structure completed

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,4 @@
-import '../styles/main.css';
-import '../styles/icons.css';
-import '../styles/page.css';
-import '../styles/task.css';
-import '../styles/task-cell.css';
-import '../styles/calendar.css';
-import '../styles/calendar-day.css';
-import '../styles/timezone-cell.css';
-import '../styles/hour-cell.css';
-import '../styles/current-time.css';
-import '../styles/button.css';
-import '../styles/select.css';
-import '../styles/calendar-status.css';
-import '../styles/header.css';
-import '../styles/profile.css';
-import '../styles/mini-calendar.css';
-import '../styles/search.css';
-import '../styles/search-result.css';
-import '../styles/events-filter.css';
-import '../styles/checkbox.css';
+import '../styles/main.scss';
 
 import { $searchPeople } from './selectors.js';
 import Search from './search.js';
